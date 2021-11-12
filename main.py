d8546ae91148c912571e6af291501f30894ef4f8
alexanderJHarris <ajharris8814@gmail.com>
2021-11-11 22:18:48 -0500

    Add main.py to project

---
+++ main.py
@@ -0,0 +1,23 @@
+from IPython.display import clear_output
+
+#Define game status whether the player wants to play or not
+def game_state():
+    pass
+
+#Define user input
+def player_choice():
+    pass
+
+#Define game display
+def display_game():
+    pass
+
+#Draw game on screen for players
+def board_create():
+    pass
+
+#Game logic
+def game_logic():
+    pass
+
+
